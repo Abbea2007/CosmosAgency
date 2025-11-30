6c116bf46d9777ac327beac64e93703578905690
Carlos Abea <carlosabea91@gmail.com>
2025-11-30 12:53:14 -0600

    Ya sirve encriptar, ya encripta las contrasenas

--- AgenciaAduanera/src/main/java/org/example/AgenciaAduanera/actions/SignInAction.java
+++ AgenciaAduanera/src/main/java/org/example/AgenciaAduanera/actions/SignInAction.java
@@ -2,33 +2,52 @@ package org.example.AgenciaAduanera.actions;
 
 import com.openxava.naviox.actions.ForwardToOriginalURIBaseAction;
 import com.openxava.naviox.impl.SignInHelper;
+import org.example.AgenciaAduanera.modelo.Seguridad.Usuario;
 import org.example.AgenciaAduanera.modelo.util.Encriptar;
 import org.openxava.jpa.XPersistence;
 import org.openxava.util.Is;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.Query;
 
 public class SignInAction extends ForwardToOriginalURIBaseAction {
 
     public void execute() throws Exception {
+
         SignInHelper.initRequest(getRequest(), getView());
         if (getErrors().contains()) return;
+
         String userName = getView().getValueString("user");
         String password = getView().getValueString("password");
+
         if (Is.emptyString(userName, password)) {
             addError("unauthorized_user");
             return;
         }
-        Long count = 0L;
-        Query query = XPersistence.getManager().createQuery("SELECT COUNT(*)" +
-                " FROM Usuario u WHERE u.username = :username AND u.password = :password ");
+
+        // Buscar usuario solo por username
+        Query query = XPersistence.getManager()
+                .createQuery("SELECT u FROM Usuario u WHERE u.username = :username");
         query.setParameter("username", userName);
-        query.setParameter("password", Encriptar.code(password));
-        count =  (Long) query.getSingleResult();
-        if (count == 0) {
+
+        Usuario usuario;
+        try {
+            usuario = (Usuario) query.getSingleResult();
+        } catch (Exception e) {
             addError("unauthorized_user");
             return;
         }
+
+        // Comparar contraseña con BCrypt
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        if (!encoder.matches(password, usuario.getPassword())) {
+            addError("unauthorized_user");
+            return;
+        }
+
+
         SignInHelper.signIn(getRequest(), userName);
         getView().reset();
         getContext().resetAllModulesExceptCurrent(getRequest());
---
+++ AgenciaAduanera/src/main/java/org/example/AgenciaAduanera/modelo/util/TestEncriptar.java
@@ -0,0 +1,8 @@
+/*package org.example.AgenciaAduanera.modelo.util;
+
+public class TestEncriptar {
+
+    public static void main(String[] args) {
+        System.out.println(Encriptar.code("2007"));
+    }
+}*/
